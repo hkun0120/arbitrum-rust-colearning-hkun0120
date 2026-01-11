49b824f3e4af459b26f450a2f721c9651eb9650a
kun <hongkun100@qq.com>
2026-01-12 00:19:29 +0800

    delete target

--- hello_web3/target/debug/deps/gas-4c30acb5b0977662.d
+++
@@ -1,5 +0,0 @@
-/Users/hong/Documents/GitHub/arbitrum-rust-colearning-hkun0120/hello_web3/target/debug/deps/gas-4c30acb5b0977662.d: src/bin/gas.rs
-
-/Users/hong/Documents/GitHub/arbitrum-rust-colearning-hkun0120/hello_web3/target/debug/deps/gas-4c30acb5b0977662: src/bin/gas.rs
-
-src/bin/gas.rs:
